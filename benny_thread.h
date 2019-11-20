d114428fb030f4ecae7e02641cfd04a3e79e47a0
Evan Newman <newmanev@oregonstate.edu>
2019-11-20 15:21:02 -0800

    copies teachers dir for setup of assignment

---
+++ benny_thread.h
@@ -0,0 +1,15 @@
+#ifndef __BENNY_THREAD_H
+# define __BENNY_THREAD_H
+
+# ifdef KTHREADS
+
+typedef ulong benny_thread_t;
+
+int benny_thread_create(benny_thread_t *, void (*func)(void*), void *);
+int benny_thread_join(benny_thread_t);
+int benny_thread_exit(int);
+int benny_thread_tid(benny_thread_t);
+
+# endif // KTHREADS
+
+#endif // __BENNY_THREAD_H
